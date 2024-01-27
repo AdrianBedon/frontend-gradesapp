c4d5330348b051922344c72dac84ddc432b1b12d
Adrián Bedón <arbedon2002@gmail.com>
2024-01-26 21:45:55 -0500

    Prueba deploy

--- src/GradesApp.jsx
+++ src/GradesApp.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { GradesPage } from "./pages/GradesPage";
 import { GradeRoutes } from "./routes/GradeRoutes";
 
@@ -6,6 +6,7 @@ export const GradesApp = () => {
   return (
     <Routes>
       <Route exact path="*" element={<GradeRoutes />} />
+      <Route exact path="*" element={<Navigate to="*" />} />
     </Routes>
   );
 };
