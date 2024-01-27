da6a004f8b17ba14b76a86dacfa52685f70d4adb
Adrián Bedón <arbedon2002@gmail.com>
2024-01-26 21:52:56 -0500

    Prueba Deploy

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { GradesApp } from "./GradesApp.jsx";
 import "./styles.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <GradesApp />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
